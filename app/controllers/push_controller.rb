d71c3e9f1d27f92a17a1821028e41b1a18f00a9d
todd <todd@todd.(none)>
2015-01-19 11:27:26 +0500

    DRY

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -3,20 +3,20 @@ class PushController < ApplicationController
 #SK= fNRmxPbURLcYdAhRXJ8RNLgbfRQ476iclF3Wqm8w
 # This is strictly used to send push notifications
 
-  def self.push_message_to_user(message, user, type, id, url, url_medium, url_thumb)
-    self.push_message_to_users(message, [user], type, id, url, url_medium, url_thumb)
+  def self.push_message_to_user(message, user, type, id)
+    self.push_message_to_users(message, [user], type, id)
   end
 
-  def self.push_message_to_users(message, users, type, id, url, url_medium, url_thumb)
+  def self.push_message_to_users(message, users, type, id)
     APNS.host = 'gateway.sandbox.push.apple.com'
-    APNS.pem = "#{Rails.root}/certi.pem"
+    APNS.pem = "#{Rails.root}/cert.pem"
     APNS.port = 2195
 
     notifications = []
     users.each do |user|
       if !user.device_token.nil?
         user.update_attribute(:notification_count, (user.notification_count).to_i + 1)
-        notification = APNS::Notification.new(user.device_token, :alert => message, :badge => user.notification_count, :other => {:type => type, :url_original => url})
+        notification = APNS::Notification.new(user.device_token, :alert => message, :badge => user.notification_count, :other => {:type => type, :id => id})
         notifications << notification
       end
     end
